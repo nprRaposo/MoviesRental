38cf18dd2a75da64da2f9bb02cb467d9ab2a57fd
nprraposo <nikoraposo@gmail.com>
2017-11-17 17:22:59 -0300

    test

--- MoviesRental/Models/Movie.cs
+++ MoviesRental/Models/Movie.cs
@@ -8,6 +8,6 @@ namespace MoviesRental.Models
     public class Movie
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } 
     }
 }
